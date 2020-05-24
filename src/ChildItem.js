32e70e37702d5a7e8a08b8a91b1a4beb170f2486
Kiran Shinde <kpshinde25@gmail.com>
2020-05-24 15:22:55 +0530

    Fixing height prop

---
+++ src/ChildItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {TouchableOpacity, Image, StyleSheet} from 'react-native';
+
+export default (ChildItem = ({
+  item,
+  style,
+  onPress,
+  index,
+  imageKey,
+  local,
+  height,
+}) => {
+  return (
+    <TouchableOpacity style={styles.container} onPress={() => onPress(index)}>
+      <Image
+        style={[styles.image, style, {height: height}]}
+        source={local ? item[imageKey] : {uri: item[imageKey]}}
+      />
+    </TouchableOpacity>
+  );
+});
+
+const styles = StyleSheet.create({
+  container: {},
+  image: {
+    height: 230,
+    resizeMode: 'stretch',
+  },
+});
